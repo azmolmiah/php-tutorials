31a473307ae607a82b959c3ed7962ef2a3938efd
azmolmiah <azmol.miah.general@gmail.com>
2022-08-22 12:46:09 +0100

    oop, file system, curl

--- php-crash-course-2020-master/12_oop/index.php
+++ php-crash-course-2020-master/12_oop/index.php
@@ -1,6 +1,21 @@
 <?php
 
+require_once "Person.php";
 // What is class and instance
+
+$p = new Person('Brad', 'Traversy');
+$p->setAge(30);
+echo '<pre>';
+var_dump($p);
+echo '</pre>';
+echo $p->getAge();
+
+$p2 = new Person('John', 'Doe');
+echo '<pre>';
+var_dump($p2);
+echo '</pre>';
+echo Person::getCounter();
+
 
 // Create Person class in Person.php
 
