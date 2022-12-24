b042a9729a599804341a38a391de7b57ead14ae4
azmolmiah <azmol.miah.general@gmail.com>
2022-12-24 19:19:44 +0000

    Add view response layouts

--- phpmvcframework/core/Router.php
+++ phpmvcframework/core/Router.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace app\core;
+
 class router
 {
 
     protected array $routes = [];
     public Request $request;
-    public function __construct(Request $request)
+    public Response $response;
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -21,11 +24,34 @@ class router
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
-        if($callback === false)
-        {
-            echo "Note found";
-            exit;
+        if ($callback === false) {
+            $this->response->setStatusCode(404);
+            return "Note found";
         }
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            return $this->renderView($callback);
+        }
+        return call_user_func($callback);
+    }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+        return ob_get_clean();
     }
 }
